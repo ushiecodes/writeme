2e34ceafbe3ce332148b224c4e3c5b22e483eaa8
Utkarsh <mscids2024.utkarsh@unigoa.ac.in>
2026-03-15 14:23:06 +0530

    feat(config): add function to save API Key

--- config.py
+++ config.py
@@ -5,3 +5,9 @@ from dotenv import load_dotenv
 
 CONFIG_DIR = Path.home() / ".config" / "turtell"
 CONFIG_FILE = CONFIG_DIR / "config.json"
+
+def save_api_key(api_key: str) -> None:
+    CONFIG_DIR.mkdir(parents=True, exist_ok=True)
+    with open(CONFIG_FILE, "w") as f:
+        json.dump({"api.key": api_key}, f)
+
